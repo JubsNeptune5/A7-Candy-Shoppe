f2cb93fa60a828c1aa7f8c45a600bf8579814bcc
JubsNeptune5 <lavertyhunter@gmail.com>
2017-12-13 16:10:58 -0500

    Home,Dec 13

--- src/Cookie.java
+++ src/Cookie.java
@@ -6,7 +6,7 @@
 
 /**
  *
- * @author CHANGE_THIS_TO_YOUR_NAME
+ * @author Hunter Laverty
  */
 public class Cookie extends DessertItem {
     
@@ -21,18 +21,36 @@ public class Cookie extends DessertItem {
         
     }
 
-    
+/**
+ * Print out the information for the receipt
+ * @return the strings for the receipt
+ */
     @Override
     public String toString(){
-         String output = "---------\n";
-        output += this.number +" @ " + this.pricePer12+ "\n";
-        output += this.name + "\n";
+        String output =  "";
+        //Display the number of cookies and the price per dozen on the same line
+        output += this.number +" @ $" + DessertShoppe.cents2dollarsAndCents(pricePer12)+ " /dz\n";
+        //NEXT LINE
+        //Display the name 
+        output += this.name;
+        //Turn the cost int in cents to a string in dollars
+        String totalcost = DessertShoppe.cents2dollarsAndCents(getCost());
+        //Print out a space between the name and where the cost needs to be
+        for (int i = this.name.length(); i < DessertShoppe.RECEIPT_WIDTH - totalcost.length()-1; i++){
+            output+=" ";
+        }
+        //Print outt he cost string with the dollar sign
+        output += "$"+totalcost;
         return output;
     }
-
+/**
+ * Get the cost with the  number of cookies nd the price per dozen
+ * @return the cost for the number of cookies in cents
+ */
     @Override
     public int getCost() {
-        return number * pricePer12;
+        //multiply the number of cokkies by the price per dozen divided by 12
+        return number * (pricePer12/12);
     }
     
 }
